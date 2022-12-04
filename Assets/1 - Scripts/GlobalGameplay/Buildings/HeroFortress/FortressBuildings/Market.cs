beaffbd4550a13129f9372732edfee2e708c386a
maxwad <maxwad@yandex.ru>
2022-12-04 22:30:26 +0300

    heros fortress 5

--- Assets/1 - Scripts/GlobalGameplay/Buildings/HeroFortress/FortressBuildings/Market.cs
+++ Assets/1 - Scripts/GlobalGameplay/Buildings/HeroFortress/FortressBuildings/Market.cs
@@ -11,36 +11,43 @@ public class Market : MonoBehaviour
     private FortressBuildings allBuildings;
     private ResourcesManager resourcesManager;
     public Dictionary<ResourceType, Sprite> resourcesIcons;
+    public Dictionary<int, ResourceType> resources = new Dictionary<int, ResourceType>();
 
     [SerializeField] private TMP_Text rateUI;
 
     [SerializeField] private Toggle[] playersStorage;
     [SerializeField] private Toggle[] marketsStorage;
-    [SerializeField] private ToggleGroup players;
-    [SerializeField] private ToggleGroup markets;
 
     [SerializeField] private Toggle[] typeOfDeal;
     [SerializeField] private Slider playersSlider;
     [SerializeField] private Slider marketsSlider;
-    [SerializeField] private TMP_Text playersSumm;
-    [SerializeField] private TMP_Text marketsSumm;
+    [SerializeField] private TMP_Text playersSum;
+    [SerializeField] private TMP_Text marketsSum;
     [SerializeField] private Image playersResource;
     [SerializeField] private Image marketsResource;
     [SerializeField] private Color errorColor;
     [SerializeField] private Color normalColor;
 
+    private ResourceType currentPlayersRes;
+    private ResourceType currentMarketsRes;
+
+    private float currentMaxAmount = 0;
+    private float playerGivesAmount = 0;
+    private float marketGivesAmount = 0;
 
     private float exchangeRateBase = 100f;
     private float exchangeRate = 10f;
     private float currentRate = 0f;
+    private float minRate = 0.1f;
 
-    private float timeDiscountBase = 0.05f;
     private float timeDiscountCurrent = 0f;
+    private float inflationStep = 0.05f;
     private float inflationPortion = 1000f;
+    private float currentInflation = 0;
+
     private float marketDiscount = 0f;
 
-    private float inRate = 0f;
-    private float outRate = 0f;
+    private float rate = 0f;
 
     public void Init()
     {
@@ -50,59 +57,144 @@ public class Market : MonoBehaviour
             allBuildings = GlobalStorage.instance.fortressBuildings;
             resourcesManager = GlobalStorage.instance.resourcesManager;
             resourcesIcons = resourcesManager.GetAllResourcesIcons();
+            resources.Add(0, ResourceType.Gold);
+            resources.Add(1, ResourceType.Food);
+            resources.Add(2, ResourceType.Wood);
+            resources.Add(3, ResourceType.Stone);
+            resources.Add(4, ResourceType.Iron);
         }
 
         gameObject.SetActive(true);
+
         ResetForm();
-
-        marketDiscount = allBuildings.GetBonusAmount(CastleBuildings.Market);
-        CalculateRate();
-        rateUI.text = Mathf.Round(currentRate).ToString();
-    }
-
-    private void CalculateRate()
-    {
-        //float timeDiscount = fortress.GetMarketPause();
-        outRate = exchangeRate - (exchangeRate * marketDiscount);
-        currentRate = outRate;
-        //Debug.Log(outRate);
-        //if(currentRate == 0) currentRate = outRate;
-
-        //int discountCout = fortress.GetMarketPause();
-        //for(int i = 0; i < discountCout; i++)
-        //{
-        //    currentRate = currentRate + outRate * timeDiscountBase;
-        //    if(currentRate >   outRate) 
-        //    {
-        //        currentRate = outRate;
-        //        break;
-        //    }
-        //}
     }
 
     private void ResetForm()
     {
-        foreach(var item in playersStorage)
-            item.isOn = false;
+        foreach(var slot in playersStorage)
+            slot.isOn = false;
 
-        foreach(var item in marketsStorage)
-            item.isOn = false;
+        foreach(var slot in marketsStorage)
+            slot.isOn = false;
 
         playersStorage[0].isOn = true;
+        currentPlayersRes = resources[0];
+
         marketsStorage[1].isOn = true;
+        currentMarketsRes = resources[1];
 
         marketsStorage[0].interactable = false;
 
         playersResource.sprite = resourcesIcons[ResourceType.Gold];
         marketsResource.sprite = resourcesIcons[ResourceType.Food];
 
-        playersSumm.text = "0";
-        marketsSumm.text = "0";
+        playerGivesAmount = 0;
+        marketGivesAmount = 0;
+        playersSum.text = "0";
+        marketsSum.text = "0";
 
         playersSlider.value = 0;
-        marketsSlider.value = 0;
+        //marketsSlider.value = 0;
 
-        playersSlider.gameObject.SetActive(typeOfDeal[0].isOn);
-        marketsSlider.gameObject.SetActive(!typeOfDeal[0].isOn);
+        //playersSlider.gameObject.SetActive(typeOfDeal[0].isOn);
+        //marketsSlider.gameObject.SetActive(!typeOfDeal[0].isOn);
+
+        CalculateRate();
+
+        rateUI.color = (currentRate < exchangeRate) ? errorColor : normalColor;
+        rateUI.text = currentRate.ToString();
+
+        Debug.Log("Infl = " + currentInflation + "; rate = " + rate);
+    }
+
+    private void CalculateRate()
+    {
+        marketDiscount = allBuildings.GetBonusAmount(CastleBuildings.Market);
+
+        //"-" because we have negative parameter
+        rate = exchangeRate - (exchangeRate * marketDiscount);
+
+        float inflationCount = currentInflation / inflationPortion;
+        int marketPause = fortress.GetMarketPause();
+        float difference = inflationCount - marketPause;
+
+        if(difference <= 0)
+        {
+            currentInflation = 0f;
+        }
+        else
+        {
+            for(int i = 0; i < difference; i++)
+                rate -= exchangeRate * inflationStep;
+
+            currentInflation -= inflationPortion * marketPause;
+        }
+
+        if(rate < minRate) rate = minRate;
+
+        currentRate = rate;
+    }
+
+    //Toggle Buttons
+    public void ChangePlayersResource(int index)
+    {
+        currentPlayersRes = resources[index];
+        playersResource.sprite = resourcesIcons[resources[index]];
+        currentMaxAmount = resourcesManager.GetResource(currentPlayersRes);
+        playersSlider.value = 0;
+
+        int checkIndex = 0;
+        for(int i = 0; i < marketsStorage.Length; i++)
+        {
+            marketsStorage[i].interactable = true;
+            if(marketsStorage[i].isOn == true) checkIndex = i;
+        }      
+
+        if(checkIndex == index)
+        {
+            checkIndex = (index == 1) ? 0 : 1;
+            marketsStorage[checkIndex].isOn = true;
+        }
+
+        marketsStorage[index].interactable = false;
+        //ChangeResource(currentPlayersRes);
+    }
+
+    //Toggle Buttons
+    public void ChangeMarketsResource(int index)
+    {
+        currentMarketsRes = resources[index];
+        marketsResource.sprite = resourcesIcons[resources[index]];
+    }
+
+    private void  ChangeResource(ResourceType newResource)
+    {
+        //currentMarketsRes = newResource;
+        currentMaxAmount = resourcesManager.GetResource(newResource);
+        playersSlider.value = 0;
+    }
+
+    //Slider
+    public void ChangeAmount()
+    {
+        playerGivesAmount = Mathf.Ceil(playersSlider.value * currentMaxAmount);
+        playersSum.text = playerGivesAmount.ToString();
+
+        marketGivesAmount = Mathf.Ceil(playerGivesAmount * (currentRate / exchangeRateBase));
+        marketsSum.text = marketGivesAmount.ToString();
+    }
+
+    //Button
+    public void Deal()
+    {
+        if(playerGivesAmount != 0)
+        {
+            resourcesManager.ChangeResource(currentPlayersRes, -playerGivesAmount);
+            resourcesManager.ChangeResource(currentMarketsRes, marketGivesAmount);
+
+            currentInflation += playerGivesAmount;
+
+            ResetForm();
+        }
     }
 }
